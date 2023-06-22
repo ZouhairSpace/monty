50752ef1cb7afd1f19d630663f8d6677729d6c5b
ZouhairSpace <jeddi.zouhair@gmail.com>
2023-06-22 16:29:56 -0700

    finished

--- monty.h
+++ monty.h
@@ -16,7 +16,7 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO project
  */
 typedef struct stack_s
 {
@@ -50,7 +50,7 @@ extern bus_t bus;
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO project
  */
 typedef struct instruction_s
 {
